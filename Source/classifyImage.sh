41f153074d5ed9b6c288b7561bfbb0906b9678a0
yhu9 <yhu9@alaska.edu>
2017-04-16 01:18:05 -0800

    Created Categries and classification can now color with multiple

---
+++ Source/classifyImage.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+####################################################################
+#you can define your models you want to use here only takes 4
+CLASS_DIR="classification"
+model1="models/construction_waste.model"
+model2="models/treematter.model"
+model3="models/general_household.model"
+model4="models/trashbags.model"
+####################################################################
+#Main program
+
+if [ "$#" -eq 1 ]
+then
+  imageFileIn=$1
+  python execute.py $imageFileIn tmp.txt noshow
+  python parseForSVM.py tmp.txt tmp2.txt +
+  ./svm_classify tmp2.txt $model1 classification1.txt >> classification_accuracy1.txt
+  ./svm_classify tmp2.txt $model2 classification2.txt >> classification_accuracy2.txt
+  ./svm_classify tmp2.txt $model3 classification3.txt >> classification_accuracy3.txt
+  ./svm_classify tmp2.txt $model4 classification4.txt >> classification_accuracy4.txt
+  rm tmp.txt
+  rm tmp2.txt
+  
+  python stichit.py $imageFileIn classification1.txt classification2.txt classification3.txt classification4.txt
+  mv classification1.txt $CLASS_DIR/classification1.txt
+  mv classification2.txt $CLASS_DIR/classification2.txt
+  mv classification3.txt $CLASS_DIR/classification3.txt
+  mv classification4.txt $CLASS_DIR/classification4.txt
+  mv classification_accuracy1.txt $CLASS_DIR/classification_accuracy1.txt
+  mv classification_accuracy2.txt $CLASS_DIR/classification_accuracy2.txt
+  mv classification_accuracy3.txt $CLASS_DIR/classification_accuracy3.txt
+  mv classification_accuracy4.txt $CLASS_DIR/classification_accuracy4.txt
+
+else
+  echo "wrong number of arguments. Expecting 1"
+  echo "arg1 = imageFileIn"
+fi
--- Source/stichit.py
+++ Source/stichit.py
@@ -1,14 +1,45 @@
+
+#Imports
 import segmentModule
 import numpy as np
 import sys
 import cv2
 
 #Constants
-INGROUP=[0,0,0]
-OUTGROUP=[255,255,255]
-UNKNOWN=[125,125,125]
+INGROUP=np.array([255,255,255],np.uint8)
+OUTGROUP=np.array([0,0,0],np.uint8)
+UNKNOWN=np.array([125,125,125],np.uint8)
+GROUP1=np.array([0,0,255],np.uint8)
+GROUP2=np.array([0,255,0],np.uint8)
+GROUP3=np.array([255,0,0],np.uint8)
+GROUP4=np.array([255,255,255],np.uint8)
+
+GROUPS = [GROUP1,GROUP2,GROUP3,GROUP4,UNKNOWN]
+SINGLE = [INGROUP,OUTGROUP,UNKNOWN]
+
+#Local functions
+
+
+#findMax
+# - Finds the index of the maximum value in the list
+def findMax(args):
+    index = 0
+    tmp = 0
+    if type(args) != type([]):
+        return -1
+
+    for i,a in enumerate(args):
+        if a > tmp:
+            index = i
+            tmp = a
+
+    return index
+
 
 #main program
+#
+
+#When using a single classification
 if len(sys.argv) == 3:
     imageFileIn = sys.argv[1]
     classificationsIn = sys.argv[2]
@@ -25,7 +56,6 @@ if len(sys.argv) == 3:
             classifications.append(float(l))
 
     if len(classifications) == len(uniquemarkers[1:]):
-        cv2.namedWindow(imageFileIn,cv2.WINDOW_NORMAL)
 
         blank = image.copy()
         blank = blank - blank
@@ -36,28 +66,104 @@ if len(sys.argv) == 3:
             elif c <= 0:
                 blank[markers == um] = OUTGROUP
         
-        num_ingroup = (blank == INGROUP).sum()
-        num_outgroup = (blank == OUTGROUP).sum()
-        num_unknown = (blank == UNKNOWN).sum()
-        total = num_ingroup + num_outgroup + num_unknown
         
-        percentIn = 0
-        if total != 0:
-            percentIn = float(num_ingroup + num_unknown) / float(total) * 100
-        else:
-            percentIn = -1.0
+        total = 0
+        pixcounts = []
+        for group in SINGLE:
+            tmp = cv2.inRange(blank,group,group)
+            num = cv2.countNonZero(tmp)
+            pixcounts.append(num)
+            total += num
 
+        percent1 = float(pixcounts[0]) / float(total) * 100
+        percent2 = float(pixcounts[1]) / float(total) * 100
+        percent3 = float(pixcounts[2]) / float(total) * 100
+
+        cv2.namedWindow(imageFileIn,cv2.WINDOW_NORMAL)
+        cv2.namedWindow("original", cv2.WINDOW_NORMAL)
         print ""
-        print ("percentage of the image in the category: %.3f%%" % percentIn)
+        print("ingroup white:  %.5f%%" % percent1)
+        print("outgroup black: %.5f%%" % percent2)
+        print("unkown grey:    %.5f%%" % percent3)
         print ""
 
+        cv2.imshow("original",image)
         cv2.imshow(imageFileIn,blank)
         cv2.waitKey(0)
 
     else:
         print "Are you sure the classification file is for that image?"
 
+#When using multiple classifications. No more than 4
+elif len(sys.argv) > 3 and len(sys.argv) <= 6:
+    #get command line arguments
+    imageFileIn = sys.argv[1]
+    classification_names = []
+    for fname in sys.argv[2:]:
+        classification_names.append(fname)
+    
+    #recreate markers
+    image, markers = segmentModule.getSegments(imageFileIn, False)
+    uniquemarkers = np.unique(markers)
+    
+    #get classfications for segments from command line arguments
+    classifications = []
+    for fname in classification_names:
+        classifications.append([])
+        with open(fname,'r') as fin:
+            lines = fin.read().splitlines()
+            for l in lines:
+                classifications[-1].append(float(l))
+
+    #If classifications are for the same image, then start stitching according to best classifier.
+    if len(classifications[0]) == len(classifications[1]) and len(classifications[2]) == len(classifications[3]) and len(classifications[1]) == len(classifications[2]):
+        
+        #make blank image
+        blank = image.copy()
+        blank = blank - blank
+        blank[markers == -1] = UNKNOWN
+
+        #Color using max of the classifications. The max according to the svm is the one farthest from the Support vector line separating the different 2 group classification. The most positive is taken
+        for c1,c2,c3,c4,um in zip(classifications[0],classifications[1],classifications[2],classifications[3],uniquemarkers[1:]):
+            tmp = [c1,c2,c3,c4]
+            index = findMax(tmp)
+            #color according to the group index
+            blank[markers == um] = GROUPS[index]
+
+        pixcounts = []
+        total = 0
+        for g in GROUPS:
+            tmp = cv2.inRange(blank,g,g)
+            num = cv2.countNonZero(tmp)
+            pixcounts.append(num)
+            total += num
+        
+        percent1 = float(pixcounts[0]) / float(total) * 100
+        percent2 = float(pixcounts[1]) / float(total) * 100
+        percent3 = float(pixcounts[2]) / float(total) * 100
+        percent4 = float(pixcounts[3]) / float(total) * 100
+        percent5 = float(pixcounts[4]) / float(total) * 100
+        
+        cv2.namedWindow(imageFileIn,cv2.WINDOW_NORMAL)
+        cv2.namedWindow("original", cv2.WINDOW_NORMAL)
+        print ""
+        print (" construction waste (red): %.5f%%" % percent1)
+        print ("      tree matter (green): %.5f%%" % percent2)
+        print ("     general goods (blue): %.5f%%" % percent3)
+        print ("       trash bags (white): %.5f%%" % percent4)
+        print ("segment separators (grey): %.5f%%" % percent5)
+        print ""
+        cv2.imshow("original",image)
+        cv2.imshow(imageFileIn,blank)
+        cv2.waitKey(0)
+
+    else:
+        print "classifications are not the same length"
+
 else:
-    print "wrong number of arguments passed. Expecting 2:"
+    print "wrong number of arguments passed. Expecting 2 or 5:"
     print "arg1 = imageFileIn"
-    print "arg2 = classificationIn"
+    print "arg2 = classification1"
+    print "arg3 = classification2"
+    print "arg4 = classification3"
+    print "arg5 = classification4"
